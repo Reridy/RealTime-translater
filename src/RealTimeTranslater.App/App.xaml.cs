12a2c19fcc38fa9d7e70b7f795a7a6bb132a103d
Mingun Choi <113280539+Reridy@users.noreply.github.com>
2026-09-20 21:14:22 +0900

    feat: log unhandled desktop errors locally

--- src/RealTimeTranslater.App/App.xaml.cs
+++ src/RealTimeTranslater.App/App.xaml.cs
@@ -1,5 +1,70 @@
+using System.Windows;
+using System.Windows.Threading;
+using RealTimeTranslater.App.Diagnostics;
+
 namespace RealTimeTranslater.App;
 
-public partial class App : System.Windows.Application
+public partial class App : Application
 {
+    protected override void OnStartup(
+        StartupEventArgs e)
+    {
+        SessionLog.Initialize();
+
+        DispatcherUnhandledException +=
+            OnDispatcherUnhandledException;
+
+        AppDomain.CurrentDomain.UnhandledException +=
+            OnDomainUnhandledException;
+
+        TaskScheduler.UnobservedTaskException +=
+            OnUnobservedTaskException;
+
+        base.OnStartup(e);
+    }
+
+    protected override void OnExit(
+        ExitEventArgs e)
+    {
+        DispatcherUnhandledException -=
+            OnDispatcherUnhandledException;
+
+        AppDomain.CurrentDomain.UnhandledException -=
+            OnDomainUnhandledException;
+
+        TaskScheduler.UnobservedTaskException -=
+            OnUnobservedTaskException;
+
+        base.OnExit(e);
+    }
+
+    private static void OnDispatcherUnhandledException(
+        object sender,
+        DispatcherUnhandledExceptionEventArgs e)
+        => SessionLog.Error(
+            e.Exception);
+
+    private static void OnDomainUnhandledException(
+        object sender,
+        UnhandledExceptionEventArgs e)
+    {
+        if (e.ExceptionObject is Exception exception)
+        {
+            SessionLog.Error(
+                exception);
+        }
+        else
+        {
+            SessionLog.Write(
+                "ERROR",
+                e.ExceptionObject?.ToString() ??
+                "Unknown unhandled exception");
+        }
+    }
+
+    private static void OnUnobservedTaskException(
+        object? sender,
+        UnobservedTaskExceptionEventArgs e)
+        => SessionLog.Error(
+            e.Exception);
 }
